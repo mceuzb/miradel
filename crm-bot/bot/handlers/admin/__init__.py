1e1f617fa92f8fdead4fdf8df52cc2356d5b57c0
mceuzb <mceuzb@gmail.com>
2026-07-29 12:31:03 +0500

    Update __init__.py

--- crm-bot/bot/handlers/admin/__init__.py
+++ crm-bot/bot/handlers/admin/__init__.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 
-from . import approval, groups, modules
+from . import approval, channels, groups, modules
 
 router = Router(name="admin")
 router.include_router(approval.router)
 router.include_router(modules.router)
 router.include_router(groups.router)
+router.include_router(channels.router)
