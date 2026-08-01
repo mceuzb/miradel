99c08a441a0680cc7887fbc839b29fdcd3a7746b
mceuzb <mceuzb@gmail.com>
2026-08-02 04:59:09 +0500

    Update states.py

--- crm-bot/bot/utils/states.py
+++ crm-bot/bot/utils/states.py
@@ -4,6 +4,7 @@ from aiogram.fsm.state import State, StatesGroup
 class Registration(StatesGroup):
     waiting_full_name = State()
     waiting_phone = State()
+    waiting_course = State()
 
 
 class GroupCreation(StatesGroup):
