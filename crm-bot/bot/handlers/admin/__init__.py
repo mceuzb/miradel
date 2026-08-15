43d267b67e7161d2c0fcef42fccbe7e6feb67659
mceuzb <mceuzb@gmail.com>
2026-08-15 15:35:56 +0500

    Update __init__.py

--- crm-bot/bot/handlers/admin/__init__.py
+++ crm-bot/bot/handlers/admin/__init__.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 
-from . import approval, broadcast, channels, contests, groups, modules, teacher_students
+from . import approval, broadcast, channels, contests, groups, modules, remove_student, teacher_students
 
 router = Router(name="admin")
 router.include_router(approval.router)
 router.include_router(teacher_students.router)
+router.include_router(remove_student.router)
 router.include_router(modules.router)
 router.include_router(groups.router)
 router.include_router(channels.router)
