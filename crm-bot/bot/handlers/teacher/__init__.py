96e609ec684ebb8e717ab0402d5587372e91d7ba
mceuzb <mceuzb@gmail.com>
2026-08-15 14:57:58 +0500

    Update __init__.py

--- crm-bot/bot/handlers/teacher/__init__.py
+++ crm-bot/bot/handlers/teacher/__init__.py
@@ -1,7 +1,8 @@
 from aiogram import Router
 
-from . import groups, tasks
+from . import groups, students, tasks
 
 router = Router(name="teacher")
 router.include_router(groups.router)
+router.include_router(students.router)
 router.include_router(tasks.router)
